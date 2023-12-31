238398e692aded0dedd4078735c24da4c7ac0245
Vicktor007 <vicktord007@gmail.com>
2023-12-31 14:13:42 +0100

    save

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,9 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
-// Utiles
+// Utils
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
@@ -22,6 +23,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors({
+    origin: ["http://localhost:5173", "https://e-shop-frontend-green.vercel.app"],
+    credentials: true
+  }));
 
 app.use("/api/users", userRoutes);
 app.use("/api/category", categoryRoutes);
@@ -29,9 +34,9 @@ app.use("/api/products", productRoutes);
 app.use("/api/upload", uploadRoutes);
 app.use("/api/orders", orderRoutes);
 
-app.get("/api/config/paypal", (req, res) => {
-  res.send({ clientId: process.env.PAYPAL_CLIENT_ID });
-});
+// app.get("/api/config/paypal", (req, res) => {
+//   res.send({ clientId: process.env.PAYPAL_CLIENT_ID });
+// });
 
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
